d5f2c23ff3dfed4e3b11f2d77ac2a7042af0fbef
Aman-khan8 <khan.aman2411@gmail.com>
2025-08-21 23:51:13 +0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Logo from './logo'
+import Logo from './Logo'
 import Input from './Input'
 import { Link,useNavigate } from 'react-router-dom'
 import Button from './Button'
