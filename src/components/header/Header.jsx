2058aabc2787f235e2a64c110ee3c16841d7c3bb
Aman-khan8 <khan.aman2411@gmail.com>
2025-08-21 23:08:11 +0500

    Varscel error updation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../logo'
+import Logo from '../Logo'
 import { Link,useNavigate } from 'react-router-dom'
 import NavButton from './NavButton'
 function Header() {
