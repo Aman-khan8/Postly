902c9d9fe91430423c7fe4d51d59483a40d588b2
Aman-khan8 <khan.aman2411@gmail.com>
2025-08-22 01:02:54 +0500

    Varcel Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
+import LogoPic from './logoPic.png'; // import relative path from Logo.js
 
 function Logo() {
   return (
-    <div className={`h-[100%] w-[100%] `}>
-        <img src="src\components\logoPic.png" alt="logo"  className='rounded-2xl'/>
-
+    <div className="h-[100%] w-[100%]">
+      <img src={LogoPic} alt="logo" className='rounded-2xl' />
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
